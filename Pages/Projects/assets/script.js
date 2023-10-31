77918cf82a801683ecf9b32dcfdd21a1adcf7bbc
andreltlc <andrecarvalho1402@gmail.com>
2023-10-30 23:40:02 -0300

    Bulls ok

--- Pages/Projects/assets/script.js
+++ Pages/Projects/assets/script.js
@@ -1,54 +1,64 @@
-function Generete(){
-    let secretNumber ="";
-    while(secretNumber.length<4){
-        const aleatoryDigit = Math.floor(Math.random()*10).toString();
-        if(!secretNumber.includes(aleatoryDigit)){
+let secretNumber;
+let attempts = 0;
+
+function Generate() {
+    let secretNumber = "";
+    while (secretNumber.length < 4) {
+        const aleatoryDigit = Math.floor(Math.random() * 10).toString();
+        if (!secretNumber.includes(aleatoryDigit)) {
             secretNumber += aleatoryDigit;
-        } 
+        }
     }
     return secretNumber;
 }
+
 function makeGuess() {
-    const guessInput = document.getElementById("guessInput");
-    const guess = guessInput.value;
+    
+    const guess = document.getElementById("guessInput").value;
     
     if (guess.length !== 4 || !/^\d{4}$/.test(guess)) {
         document.getElementById("message").textContent = "Invalid guess. Make a new guess";
-        return;
-    }
-
-    const [bulls, cows] = rate(guess, secretNumber);
-    tentativas++;
-
-    if (bulls === 4) {
-        document.getElementById("message").textContent = "Congrulations! You got it right " + tries + " attempts.";
-        document.getElementById("guessInput").setAttribute("disabled", "true");
-        document.getElementById("guessButton").setAttribute("disabled", "true");
     } else {
-        document.getElementById("message").textContent = "Bulls: " + bulls + " Cows: " + cows;
+        const [bulls, cows] = rate(guess, secretNumber);
+        attempts++;
+
+        if (bulls === 4) {
+            document.getElementById("message").textContent = "Congrulation! Você acertou o número em " + attempts + " attempts. To play again reload the page";
+            document.getElementById("guessInput").setAttribute("disabled", "true");
+            document.getElementById("guessButton").setAttribute("disabled", "true");
+        } else {
+            document.getElementById("message").textContent = "Bulls: " + bulls + " Cows: " + cows;
+        }
+
+        guessInput.value = "";
     }
 }
 
-function rate(guess,secretNumber){
+function rate(guess, secretNumber) {
     let bulls = 0;
     let cows = 0;
-    for( let i = 0; i < 4 ;i++){
-        if(guess[i] === secretNumber[i]){
+    for (let i = 0; i < 4; i++) {
+        if (guess[i] === secretNumber[i]) {
             bulls++;
-        }else if(secretNumber.includes(guess[i])){
+        } else if (secretNumber.includes(guess[i])) {
             cows++;
         }
     }
     return [bulls, cows];
 }
 
-function playBAC(){
-    const secretNum = Generete();
-    let attempts = 0;
+function playBAC() {
+    secretNumber = Generate();
+    attempts = 0;
     document.getElementById("message").textContent = "";
     document.getElementById("guessInput").removeAttribute("disabled");
     document.getElementById("guessButton").removeAttribute("disabled");
     document.getElementById("guessInput").value = "";
 }
-document.getElementById("guessButton").addEventListener("click", makeGuess);
-window.onload = iniciarJogo;
+
+document.getElementById("guessForm").addEventListener("submit", function (e) {
+    e.preventDefault(); 
+    makeGuess();
+});
+
+window.addEventListener("load", playBAC);
